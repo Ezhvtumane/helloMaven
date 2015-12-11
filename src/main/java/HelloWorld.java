9dbde1334cafe61e829a426cdbdb74c2a2646e15
admin <gogaopel@gmail.com>
2015-12-11 16:26:18 +0300

    Recompile&Rebuild& spring fw added

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,12 @@
 package src.main.java.hello;
 
 import org.joda.time.LocalTime;
+import org.springframework.core.SpringVersion;
+import javax.swing.*;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		ID id = new ID();
 		Greeter greeter = new Greeter();
 		Pusher pusher = new Pusher();
 		System.out.println(greeter.sayHello());
@@ -11,9 +14,5 @@ public class HelloWorld {
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
 		System.out.println(greeter.sayBye());
-
-
-
-		//New comment
 	}
 }
---
+++ src/main/java/ID.java
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 12/11/15.
+ */
+
+//package src.main.java.hello;
+
+public class ID {
+
+    }
+
