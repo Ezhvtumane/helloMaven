f058cd063383e87b5d578b1a8122e63489f0db7b
admin <gogaopel@gmail.com>
2015-12-11 15:14:28 +0300

    Pusher add

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,7 +5,9 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
+		Pusher pusher = new Pusher();
 		System.out.println(greeter.sayHello());
+		System.out.println(pusher.push());
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
 		System.out.println(greeter.sayBye());
---
+++ src/main/java/Pusher.java
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 12/11/15.
+ */
+
+package hello;
+
+public class Pusher {
+    public String push()
+    {
+        return "Push you!";
+    }
+}
